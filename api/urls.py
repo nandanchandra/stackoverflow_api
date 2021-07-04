7e05cab28eefff9dda50b0178b8ffedd210d7abb
implicitdefcncdragneel <chandranandan.chandrakar@gmail.com>
2021-07-04 21:37:15 +0530

    searchUrls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from . views import All_questions_View
+from . views import *
 
 urlpatterns = [
-    url('questions/$',All_questions_View.as_view(),name='all_ques')
+    url('questions/$',All_questions_View.as_view(),name='all_ques'),
+    url('search/$',Query_ques_View.as_view(),name='search'),
 
 ]
