7cfbdaaa93fbc24e7998e7a396a813bdb4bff3dd
implicitdefcncdragneel <chandranandan.chandrakar@gmail.com>
2021-07-05 01:11:30 +0530

    task

---
+++ api/tasks.py
@@ -0,0 +1,14 @@
+from celery import *
+
+from .models import *
+
+
+def searchtask(query, apidata):
+	if Questions.objects.filter(query=query).exists()==False:
+		ques_db=Questions.objects.create(query=query, data=apidata)
+		ques_db.save()
+
+def answertask(question_id, apidata):
+	if Answers.objects.filter(question_id=question_id).exists()==False:
+		ans_db=Answers.objects.create(question_id=question_id, data=apidata)
+		ans_db.save()
--- stackoverflowapi/celery.py
+++ stackoverflowapi/celery.py
@@ -9,4 +9,8 @@ app = Celery('stackoverflowapi')
 app.config_from_object('django.conf:settings',namespace='CELERY')
 app.autodiscover_tasks()
 
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
 
